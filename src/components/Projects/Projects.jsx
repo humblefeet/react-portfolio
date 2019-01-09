8d40fb7de791a6df05a90e8ebf33727b50f38565
Matthieu Bourgois <matthieubourgois@Matthieus-MacBook-Pro.local>
2019-01-08 17:42:14 -0800

    added links for the projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -14,28 +14,28 @@ const Projects = props =>(
             <div className="module whatch">
                 <a      target="_blank"
                         rel="noopener noreferrer"
-                        href="https://github.com/ladystensberg/whatch">
+                        href="https://thawing-retreat-16282.herokuapp.com/">
                     <img className="images " src={whatch} alt="whatch screenshot"></img>
                 </a>
             </div>
             <div className="module hike">
                 <a  target="_blank"
                         rel="noopener noreferrer"
-                        href="https://github.com/humblefeet/hike-hitch">
+                        href="https://pacific-falls-21296.herokuapp.com/">
                     <img className="images " src={hikeHitch} alt="Hike Hitch screenshot"></img>
                 </a>
             </div>
             <div className="module rain">
                 <a    target="_blank"
                         rel="noopener noreferrer"
-                        href="https://github.com/humblefeet/rain-or-shine"> 
+                        href="https://glacial-wave-90784.herokuapp.com/"> 
                     <img className="images " src={rainOrShine} alt="Rain or Shine screenshot"></img>
                 </a>
             </div>
             <div className="module word">
                 <a      target="_blank"
                         rel="noopener noreferrer"
-                        href="https://github.com/humblefeet/Word-Scramble">
+                        href="https://keen-kalam-6d17d9.netlify.com/">
                     <img className="images " src={unscrambler} alt="unscrambler screenshot"></img>
                 </a>
             </div>
